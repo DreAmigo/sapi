14bbd488b7c3cf5580da8aee94d3d1c826d7c741
André Bocati <andre@randb.com.au>
2012-12-18 15:23:59 +1000

    Added extension

---
+++ extension.driver.php
@@ -0,0 +1,140 @@
+<?php
+	Class extension_SAPI extends Extension {
+
+		public static $config_handle = 'sapi';
+
+		public function getSubscribedDelegates(){
+			return array(
+				array(
+					'page'		=> '/system/preferences/',
+					'delegate'	=> 'AddCustomPreferenceFieldsets',
+					'callback'	=> 'appendPreferences'
+				),
+				array(
+					'page'		=> '/system/preferences/',
+					'delegate'	=> 'Save',
+					'callback'	=> 'savePreferences'
+				),
+			);
+		}
+
+	/*-------------------------------------------------------------------------
+		Utilities:
+	-------------------------------------------------------------------------*/
+
+		public function getSetting($key) {
+			return Symphony::Configuration()->get($key, self::$config_handle);
+		}
+
+
+	/*-------------------------------------------------------------------------
+		Preferences:
+	-------------------------------------------------------------------------*/
+
+		public function getPreferencesData() {
+			$data = array(
+				'api-key' => '',
+				'api-category' => '',
+				'api-keyword' => '',
+				'gateway-mode' => 'sandbox'
+			);
+
+			foreach ($data as $key => &$value) {
+				$value = $this->getSetting($key);
+			}
+
+			return $data;
+		}
+
+		/**
+		 * Allow the user to add their SAPI keys.
+		 *
+		 * @uses AddCustomPreferenceFieldsets
+		 */
+		public function appendPreferences($context) {
+			$data = $this->getPreferencesData();
+
+			$fieldset = new XMLElement('fieldset');
+			$fieldset->setAttribute('class', 'settings');
+			$fieldset->appendChild(new XMLElement('legend', __('SAPI Details')));
+
+			$this->buildPreferences($fieldset, array(
+				array(
+					'label' => 'API Key',
+					'name' => 'api-key',
+					'value' => $data['api-key'],
+					'type' => 'text'
+				),
+				array(
+					'label' => 'API categories',
+					'name' => 'api-category',
+					'value' => $data['api-category'],
+					'type' => 'text'
+				),
+				array(
+					'label' => 'API keyword',
+					'name' => 'api-keyword',
+					'value' => $data['api-word'],
+					'type' => 'text'
+				)
+			));
+
+			$context['wrapper']->appendChild($fieldset);
+		}
+
+
+		public function buildPreferences($fieldset, $data) {
+			$row = null;
+
+			foreach ($data as $index => $item) {
+				if ($index % 2 == 0) {
+					if ($row) $fieldset->appendChild($row);
+
+					$row = new XMLElement('div');
+					$row->setAttribute('class', 'group');
+				}
+
+				$label = Widget::Label(__($item['label']));
+				$name = 'settings[' . self::$config_handle . '][' . $item['name'] . ']';
+
+				$input = Widget::Input($name, $item['value'], $item['type']);
+
+				$label->appendChild($input);
+				$row->appendChild($label);
+			}
+            
+			// Build the Gateway Mode
+			$label = new XMLElement('label', __('Gateway Mode'));
+			$options = array(
+				array('sandbox', $this->isTesting() , __('Sandbox')),
+				array('live',  !$this->isTesting(), __('Live'))
+			);
+            
+			$label->appendChild(Widget::Select('settings[sapi][gateway-mode]', $options));
+			$row->appendChild($label);
+
+			$fieldset->appendChild($row);
+		}
+
+		public static function isTesting() {
+			return Symphony::Configuration()->get('gateway-mode', 'sapi') == 'sandbox';
+		}		
+
+		/**
+		 * Saves the SAPI to the configuration
+		 *
+		 * @uses savePreferences
+		 */
+		public function savePreferences(array &$context){
+			$settings = $context['settings'];
+
+			// Active Section
+			Symphony::Configuration()->set('api-username', $settings['sapi']['api-key'], 'sapi');
+			Symphony::Configuration()->set('api-password', $settings['sapi']['api-category'], 'sapi');
+			Symphony::Configuration()->set('api-signature', $settings['sapi']['api-keyword'], 'sapi');
+			Symphony::Configuration()->set('gateway-mode', $settings['sapi']['gateway-mode'], 'sapi');
+
+			Administration::instance()->saveConfig();
+		}
+
+	}
---
+++ lib/api.php
@@ -0,0 +1,209 @@
+<?php
+
+/**
+ * API that will return some data from sensis.com.au
+ * 
+ * @see http://developers.sensis.com.au/
+ * @author Andre Bocati <andre@randb.com.au>
+ */
+
+Class SAPI {
+    
+    /*
+     * Sandbox endpoint
+     */
+    const ENDPOINT_TEST = "http://api.sensis.com.au/ob-20110511/test/search";
+    
+    /*
+     * Live endpoint
+     */
+    const ENDPOINT_LIVE = "http://api.sensis.com.au/ob-20110511/prod/search";
+    
+
+    /**
+    * Whether or not SAPI is in test mode.
+    *
+    * @return type
+    */
+    public static function isTesting() {
+        return Symphony::Configuration()->get('gateway-mode', 'sapi') == 'sandbox';
+    }
+        
+    
+    /**
+     * Connect to the SAPI API.
+     * 
+     * @param type $currentPage
+     * @return string
+     * @throws Exception
+     */
+    public function request($currentPage = 1) {
+        
+        /*
+         * API KEY
+         * @see http://developers.sensis.com.au/
+         */
+        $apiKey = Symphony::Configuration()->get('api-key', 'sapi');
+        
+        /*
+         * @see http://developers.sensis.com.au/page/category_explorer
+         */
+        $searchOnCategories = Symphony::Configuration()->get('api-category', 'sapi');
+        //
+
+        /*
+         * Query for the API.
+         */
+        $query = Symphony::Configuration()->get('api-keyword', 'sapi');
+        
+        
+        if(self::isTesting()) {
+            $endpoint = self::ENDPOINT_TEST;
+        }
+        else {
+            $endpoint = self::ENDPOINT_LIVE;
+        }
+        
+        $url = $endpoint . "?rows=50&page=". $currentPage ."&query=". $query ."&key=" . self::API_KEY . $searchOnCategories;
+        
+        # Call the endpoint
+        $response = file_get_contents($url); 
+        
+        // Check the response.
+        if (!$response) {
+            throw new Exception("Error calling API ($http_response_header[0])");
+        }
+
+        // Convert to json.
+        $result = json_decode($response, true);
+ 
+        # grab the response code
+        $code = $result["code"];
+
+        # ensure successful status code
+        if ($code == 200) { # success
+            return $result; 
+        } else if ($code == 206) { # spell-checker was run
+            echo "Note: " . $result["message"] . "\n";
+            return result;
+        } else {
+            throw new Exception("API returned error: " . 
+                    $result["message"] . ", code: " . $result["code"]);
+        }
+
+    }
+    
+    /**
+     * Build an array of data.
+     * 
+     * @param type $data A result from the first query that we can receive the total of pages to run in the loop.
+     * @return array Return an array of all the data from the search.
+     */
+    public function buildResult($data) {
+
+        // Get some details from the first request.
+        $totalResults = $data["totalResults"];
+        $currentPage = $data["currentPage"];
+        $totalPages = $data["totalPages"];
+        $count = $data["count"];
+        
+        // Array for all results of all pages.
+        $results = array();
+        
+        // Having the number of pages lets request the results for every single page.
+        for ($page = 1; $page <= $totalPages; $page++) {
+
+            // Query results for the $page.
+            $query = $this->request($page);
+            
+            // Go through all results of the $page.
+            foreach ($query['results'] as $result) {
+               
+                $id = $result["id"];
+                $name = $result["name"];
+                $state = $result['primaryAddress']['state'];
+                $type = $result['primaryAddress']['type'];
+                $suburb = $result['primaryAddress']['suburb'];
+                $postcode = $result['primaryAddress']['postcode'];
+                $addressLine = $result['primaryAddress']['addressLine'];                
+                $description = $result['mediumDescriptor'];
+                
+                // There is in the API primary and secondary contacts.
+                $email = array();
+                $url   = array();
+                $phone = array();
+                $anotherContact = array();
+                
+                // Grab the primary contacts.
+                if($result['primaryContacts']) {
+                    foreach($result['primaryContacts'] as $key => $contacts) {
+                        // Look for all types of contacts.
+                        switch ($contacts['type']) {
+                            case "EMAIL":
+                                $email[] = $contacts['value'];
+                            break;
+                            case "URL":
+                                $url[] = $contacts['value'];
+                            break;
+                            case "PHONE":
+                                $phone[] = $contacts['value'];
+                            break;
+                            default:
+                                $anotherContact[] = $contacts['type'] . ": ". $contacts['value'];
+                            break;
+                        }
+                    }
+                }
+                
+                // Grab the secondary contacts.     
+                if($result['secondaryContacts']) {
+                    foreach($result['secondaryContacts'] as $key => $contacts) {
+                        // Look for all types of contacts.
+                        switch ($contacts[0]['type']) {
+                            case "PHONE":
+                                $phone[] = $contacts[0]['value'];
+                            break;
+                            case "EMAIL":
+                                $email[] = $contacts[0]['value'];
+                            break;
+                            default:
+                                $anotherContact[] = $contacts[0]['type'] . ": ". $contacts[0]['value'];
+                            break;
+                        }
+                    }
+                }
+                
+                // Build a result array.
+                $results[] = array (
+                    'id'    => $id,
+                    'name'  => $name,
+                    'description' => $description,
+                    'address' => $addressLine . ", " . $suburb . ", " . $state . ", " . $postcode,
+                    'phone' => $phone,
+                    'email' => $email,
+                    'anotherContacts' => $anotherContact,
+                    'allData' => $result
+                );
+                
+            }
+        }
+        
+        return $results;
+        
+    }
+    
+    /**
+     * Do a search in teh SAPI API.
+     * 
+     * @return type
+     */
+    public function doSearch() {
+
+        // Try to do a first search to find out how many pages and results are in the query.
+        $results = $this->request();
+
+        // Build the array of results.
+        return $this->buildResult($results);
+    }
+    
+}
